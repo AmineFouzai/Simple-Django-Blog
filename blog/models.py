93f9737dd931639ddaa177f9c3430fc2c8b3c008
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-05-11 20:21:19 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class post(models.Model):
-    id=models.IntegerField(primary_key=True)
+  
     title=models.CharField(max_length=200)
     body=models.TextField()
     author=models.CharField(max_length=20)
